80459812d7fe2aaec263de768743525ff12935ff
Matthew Hootman <mhootman1980@gmail.com>
2014-12-28 20:08:37 -0600

    sorting works, arrows no show

--- app/controllers/shopkins_controller.rb
+++ app/controllers/shopkins_controller.rb
@@ -1,8 +1,10 @@
 class ShopkinsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   # GET /shopkins
   # GET /shopkins.json
   def index
-    @shopkins = Shopkin.page(params[:page]).per(10)
+    @shopkins = Shopkin.order(sort_column + ' ' + sort_direction).page(params[:page]).per(10)
   end
 
   # GET /shopkins/1
@@ -15,5 +17,13 @@ class ShopkinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def shopkin_params
       params.require(:shopkin).permit(:name, :rating, :rarity, :series, :team)
+    end
+
+    def sort_column
+      Shopkin.column_names.include?(params[:sort]) ? params[:sort] : 'name'
+    end
+
+    def sort_direction
+      %w(asc desc).include?(params[:direction]) ?  params[:direction] : 'asc'
     end
 end
